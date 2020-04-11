555f728e84356f0800dd8bb0328f23af0c9bc2ea
loheagn <linan_123@buaa.edu.cn>
2020-04-11 22:06:37 +0800

    initial commit

---
+++ session-server/src/test/java/com/kinlim/sessionserver/SessionServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kinlim.sessionserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SessionServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
