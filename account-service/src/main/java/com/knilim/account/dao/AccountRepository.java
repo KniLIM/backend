decd66b10195b9cb45381e09987950be4df0723c
ChetTian <724133160@qq.com>
2020-04-18 17:34:46 +0800

    modify userinfor and pw added

--- account-service/src/main/java/com/knilim/account/dao/AccountRepository.java
+++ account-service/src/main/java/com/knilim/account/dao/AccountRepository.java
@@ -9,4 +9,32 @@ public interface AccountRepository {
      * @param user  账户信息
      */
     boolean insert(User user) ;
+
+    /**
+     * 修改用户信息
+     * @param user
+     * @return
+     */
+    boolean updateUserInformation(User user) ;
+
+    /**
+     * 修改密码
+     * @param user
+     * @return
+     */
+    boolean updatePassword(User user, String password) ;
+
+    /**
+     * 根据id返回用户信息
+     * @param id
+     * @return
+     */
+    User searchById(String id) ;
+
+    /**
+     * 根据关键字返回用户信息
+     * @param keyword
+     * @return
+     */
+    User searchByKeyword(String keyword) ;
 }
