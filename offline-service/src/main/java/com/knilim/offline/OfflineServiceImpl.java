8b52e0bb8eaab2b36b7919ee04c2cbf7a31ae2cc
inskil <pj0713@126.com>
2020-06-22 04:28:58 +0800

    add offline debug info

--- offline-service/src/main/java/com/knilim/offline/OfflineServiceImpl.java
+++ offline-service/src/main/java/com/knilim/offline/OfflineServiceImpl.java
@@ -2,6 +2,8 @@ package com.knilim.offline;
 
 import com.knilim.service.OfflineService;
 import org.apache.dubbo.config.annotation.Service;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.stereotype.Component;
 
@@ -16,14 +18,21 @@ public class OfflineServiceImpl implements OfflineService {
     @Resource
     private RedisTemplate<String, Byte[]> template;
 
+    private static final Logger logger = LoggerFactory.getLogger(OfflineServiceImpl.class);
+
+
     @Override
     public List<Byte[]> getOfflineMsgs(String userId) {
+
         List<Byte[]> msgs = template.boundListOps(userId).range(0, -1);
+        logger.info("getOfflineMsgs : from redis msg[{}]",msgs);
+
         if (msgs != null) {
             template.delete(userId);
+            logger.info("getOfflineMsgs : final redis msg[{}]",msgs);
             return msgs;
         }
-
+        logger.info("getOfflineMsgs : final redis msg is empty");
         return new ArrayList<>();
     }
 
